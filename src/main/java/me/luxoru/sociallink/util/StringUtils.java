2386b630f54fe51668826e4704cf021fa42f0af4
Luxoru <luxoru.dev@gmail.com>
2024-07-13 21:18:36 +0100

    Added StringUtils

---
+++ src/main/java/me/luxoru/sociallink/util/StringUtils.java
@@ -0,0 +1,33 @@
+package me.luxoru.sociallink.util;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class StringUtils {
+
+
+    public static String generateRandomString(int length) {
+        return generateRandomString(length, true);
+    }
+
+    public static String generateRandomString(int length, boolean isUsingNumbers){
+
+         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+         String numbers = "0123456789";
+
+        ThreadLocalRandom rand = ThreadLocalRandom.current();
+
+        StringBuilder sb = new StringBuilder(length);
+
+        if(isUsingNumbers){
+            characters += numbers;
+        }
+
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(rand.nextInt(characters.length())));
+        }
+
+        return sb.toString();
+
+    }
+
+}
