b311dda6b8292e2f09430173ca94cc135a103d8f
Luxoru <luxoru.dev@gmail.com>
2024-07-16 23:32:05 +0100

    minor changes

--- src/main/java/me/luxoru/sociallink/user/SocialPlayerManager.java
+++ src/main/java/me/luxoru/sociallink/user/SocialPlayerManager.java
@@ -1,5 +1,8 @@
 package me.luxoru.sociallink.user;
 
+import me.luxoru.sociallink.player.ServerPlayer;
+
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -17,14 +20,42 @@ public class SocialPlayerManager {
         this.players.add(socialPlayer);
     }
 
+    public void addPlayersIfAbsent(Collection<SocialPlayer> socialPlayers){
+        for(SocialPlayer socialPlayer : socialPlayers){
+            if(socialPlayer == null){
+                //Shouldnt really be happening.
+                continue;
+            }
+            addPlayerIfAbsent(socialPlayer);
+        }
+    }
+
+    public void addPlayerIfAbsent(SocialPlayer socialPlayer) {
+        this.players.add(socialPlayer);
+        ServerPlayer.getManager().addPlayer(socialPlayer.getServerPlayer());
+    }
+
     public void removePlayer(SocialPlayer socialPlayer) {
         this.players.remove(socialPlayer);
+    }
+
+    public void removePlayer(UUID uuid) {
+        this.players.removeIf(player -> player.getServerPlayer().getUuid().equals(uuid));
     }
 
     public SocialPlayer getSocialPlayer(String playerName) {
 
         for (SocialPlayer player : this.players) {
             if(player.getServerPlayer().getName().equals(playerName)) {
+                return player;
+            }
+        }
+        return null;
+    }
+
+    public SocialPlayer getSocialPlayer(SocialPlayer socialPlayer) {
+        for (SocialPlayer player : this.players) {
+            if(socialPlayer.equals(player)) {
                 return player;
             }
         }
@@ -41,5 +72,13 @@ public class SocialPlayerManager {
         return null;
     }
 
+    public SocialPlayer getSocialPlayer(ServerPlayer serverPlayer) {
 
+        for (SocialPlayer player : this.players) {
+            if(player.getServerPlayer().equals(serverPlayer)) {
+                return player;
+            }
+        }
+        return null;
+    }
 }
