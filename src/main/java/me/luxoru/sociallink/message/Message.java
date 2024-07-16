72dfd480e15af57fc3c756097cd0a7c7ecf665a8
Luxoru <luxoru.dev@gmail.com>
2024-07-16 23:29:22 +0100

    Created message handler for when player joins

---
+++ src/main/java/me/luxoru/sociallink/message/Message.java
@@ -0,0 +1,12 @@
+package me.luxoru.sociallink.message;
+
+/**
+ * Dummy class
+ * All messages sent when a player joins a new server.
+ */
+
+public interface Message {
+
+     default void send(){};
+
+}
