d65929cea42612f39ca63de13c0650e708dda47a
Luxoru <luxoru.dev@gmail.com>
2024-07-25 23:13:36 +0100

    Added getting and saving of data

--- src/main/java/me/luxoru/sociallink/user/SocialPlayer.java
+++ src/main/java/me/luxoru/sociallink/user/SocialPlayer.java
@@ -5,57 +5,44 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 import me.luxoru.sociallink.SocialLink;
-import me.luxoru.sociallink.commands.friend.redis.FriendRequestCommand;
-import me.luxoru.sociallink.data.redis.RedisDatabaseAdapter;
 import me.luxoru.sociallink.data.redis.RedisObject;
+import me.luxoru.sociallink.data.redis.RedisRepository;
+import me.luxoru.sociallink.friend.FriendManager;
 import me.luxoru.sociallink.message.Message;
 import me.luxoru.sociallink.message.MessageAdapterFactory;
-import me.luxoru.sociallink.party.Party;
+import me.luxoru.sociallink.message.MessageManager;
 import me.luxoru.sociallink.player.ServerPlayer;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Type;
 import java.util.*;
 
 
 @Getter
 public class SocialPlayer extends RedisObject {
+
+
     private  ServerPlayer serverPlayer;
-    private  FriendManager friendManager;
-    @Setter
-    private  SocialLink plugin;
-    private Set<Message> messages;
-    private Party party;
+    private FriendManager friendManager;
+
+    private MessageManager messageManager;
+
+    @Getter
+    private static final SocialPlayerManager manager = new SocialPlayerManager();
 
     private final Gson gson = new GsonBuilder()
             .registerTypeAdapterFactory(new MessageAdapterFactory())
             .create();
 
     public SocialPlayer(){
-        RedisDatabaseAdapter.INSTANCE.getManager().addCommand(FriendRequestCommand.class);
-        RedisDatabaseAdapter.INSTANCE.getManager().addCallback(FriendRequestCommand.class, (friendRequest) ->{
-            String requesterName = friendRequest.getRequesterName();
-            Player recipientPlayer = Bukkit.getPlayer(friendRequest.getReceiverId());
-            System.out.println(recipientPlayer);
-            if(recipientPlayer == null) {
-                //Not in this server
-                return;
-            }
 
-            recipientPlayer.sendMessage(requesterName+" sent you a friend request :)");
-
-        });
     }
 
-    public SocialPlayer(ServerPlayer serverPlayer, SocialLink plugin){
+    public SocialPlayer(ServerPlayer serverPlayer){
         this();
         this.serverPlayer = serverPlayer;
-        this.plugin = plugin;
-        this.friendManager = new FriendManager(this.getServerPlayer().getUuid());
-        this.messages = new HashSet<>();
+        this.friendManager = new FriendManager(this, getServerPlayer().getUuid());
+        this.messageManager = new MessageManager();
     }
 
 
@@ -69,33 +56,43 @@ public class SocialPlayer extends RedisObject {
     public void construct(@NonNull Map<String, String> data) {
         serverPlayer = gson.fromJson(data.get("serverplayer"), new TypeToken<ServerPlayer>(){}.getType());
         friendManager = gson.fromJson(data.get("friendmanager"), new TypeToken<FriendManager>(){}.getType());
-        Type messageSetType = new TypeToken<Set<Message>>() {}.getType();
-        messages = gson.fromJson(data.get("messages"), new TypeToken<Set<Message>>(){}.getType());
-        plugin = SocialLink.INSTANCE;
+        messageManager = new MessageManager();
+        messageManager.setMessages(gson.fromJson(data.get("messages"), new TypeToken<Set<Message>>(){}.getType()));
+
     }
+
+
 
     @Override
     public @NonNull Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
         map.put("serverplayer", gson.toJson(serverPlayer));
         map.put("friendmanager", gson.toJson(friendManager));
-        map.put("messages", gson.toJson(messages));
+        map.put("messages", gson.toJson(messageManager.getMessages()));
         return map;
     }
 
-
-    public static SocialPlayer getSocialPlayer(UUID playerUUID){
-        SocialPlayer socialplayer = SocialLink.INSTANCE.getRedisRepository().getObject("socialplayer", SocialPlayer.class, playerUUID.toString());
-        if(socialplayer == null){
-            return null;
-        }
-        System.out.println("Retrieved social player " + playerUUID);
-        return socialplayer;
+    @Override
+    public SocialPlayer save() {
+        RedisRepository.INSTANCE.insert("sociallink.socialplayer", this);
+        return this;
     }
-    
+
+    public SocialPlayer sync() {
+        SocialPlayer fetchedPlayer = RedisRepository.INSTANCE.getObject("sociallink.socialplayer", SocialPlayer.class, this.getRedisId());
+
+        if (fetchedPlayer != null) {
+            this.serverPlayer = fetchedPlayer.getServerPlayer();
+            this.friendManager = fetchedPlayer.getFriendManager();
+            this.messageManager = fetchedPlayer.getMessageManager();
+        }
+
+        return this;
+    }
+
 
     public static SocialPlayer getOrCreateSocialPlayer(Player player, SocialLink plugin){
-        SocialPlayer socialplayer = plugin.getRedisRepository().getObject("socialplayer", SocialPlayer.class, player.getUniqueId().toString());
+        SocialPlayer socialplayer = RedisRepository.INSTANCE.getObject("sociallink.socialplayer", SocialPlayer.class, player.getUniqueId().toString());
         if(socialplayer == null){
             System.out.println("Creating social player " + player.getUniqueId());
             return createSocialPlayer(player, plugin);
@@ -107,21 +104,10 @@ public class SocialPlayer extends RedisObject {
 
     private static SocialPlayer createSocialPlayer(Player player, SocialLink plugin){
         ServerPlayer serverPlayer = new ServerPlayer(player.getUniqueId(), player.getName(), plugin.getServerName(), true);
-        SocialPlayer socialPlayer = new SocialPlayer(serverPlayer, plugin);
-        plugin.getRedisRepository().insert("socialplayer", socialPlayer);
+        SocialPlayer socialPlayer = new SocialPlayer(serverPlayer);
+        socialPlayer.save();
 
         return socialPlayer;
     }
 
-    public void addMessage(Message message){
-        messages.add(message);
-    }
-
-    public void removeMessage(Message message){
-        messages.remove(message);
-    }
-
-    public Set<Message> getMessages() {
-        return Set.copyOf(messages);
-    }
 }
