3f7f42c5e4a65a0293ed6a7e859b7d30115a8f04
Luxoru <luxoru.dev@gmail.com>
2024-07-25 23:19:06 +0100

    Forgot to set status of player...

--- src/main/java/me/luxoru/sociallink/listener/PlayerJoinLeaveListener.java
+++ src/main/java/me/luxoru/sociallink/listener/PlayerJoinLeaveListener.java
@@ -2,9 +2,13 @@ package me.luxoru.sociallink.listener;
 
 
 import me.luxoru.sociallink.SocialLink;
+import me.luxoru.sociallink.data.redis.TimeToLiveRule;
 import me.luxoru.sociallink.message.Message;
 import me.luxoru.sociallink.message.friend.FriendRequestPendingMessage;
+import me.luxoru.sociallink.player.ServerPlayer;
+import me.luxoru.sociallink.player.ServerPlayerManager;
 import me.luxoru.sociallink.user.SocialPlayer;
+import me.luxoru.sociallink.user.SocialPlayerManager;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,22 +19,28 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class PlayerJoinLeaveListener implements Listener {
 
     private final SocialLink plugin;
+    private SocialPlayerManager manager;
 
     public PlayerJoinLeaveListener(SocialLink plugin) {
         this.plugin = plugin;
+        this.manager = SocialPlayer.getManager();
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
 
+        Player player = event.getPlayer();
         SocialPlayer socialPlayer = SocialPlayer.getOrCreateSocialPlayer(player, plugin);
-        plugin.getSocialPlayerManager().addPlayer(socialPlayer);
+        socialPlayer.getServerPlayer().setOnline(true);
+        socialPlayer.save();
+        manager.addPlayer(socialPlayer, TimeToLiveRule.FOREVER);
+
         int count = 0;
-        for(Message message : socialPlayer.getMessages()){
+        for(Message message : socialPlayer.getMessageManager().getMessages()){
             if(message instanceof FriendRequestPendingMessage friendRequestPendingMessage){
+                if(friendRequestPendingMessage.isSender(player.getUniqueId()))continue;
                 if(friendRequestPendingMessage.hasExpired()){
-                    socialPlayer.removeMessage(message);
+                    socialPlayer.getMessageManager().removeMessage(message.getMessageUUID());
                     continue;
                 }
                 count++;
@@ -46,18 +56,18 @@ public class PlayerJoinLeaveListener implements Listener {
 
         }
 
+
     }
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event) {
         Player player = event.getPlayer();
 
-        SocialPlayer socialPlayer = plugin.getSocialPlayerManager().getSocialPlayer(player.getUniqueId());
-        plugin.getRedisRepository().insert("socialplayer", socialPlayer);
-
-        plugin.getSocialPlayerManager().removePlayer(player.getUniqueId());
-
-
+        SocialPlayer socialPlayer = manager.getSocialPlayer(player.getUniqueId());
+        socialPlayer.getServerPlayer().setOnline(false);
+        socialPlayer.save();
+        ServerPlayer.getManager().removePlayer(socialPlayer.getServerPlayer());
+        manager.removePlayer(socialPlayer);
     }
 
 }
