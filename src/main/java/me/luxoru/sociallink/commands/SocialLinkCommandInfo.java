af74cb8f01ab66fcc684c784016d003a70a918e1
Luxoru <luxoru.dev@gmail.com>
2024-07-25 23:10:41 +0100

    Added aliases

--- src/main/java/me/luxoru/sociallink/commands/SocialLinkCommandInfo.java
+++ src/main/java/me/luxoru/sociallink/commands/SocialLinkCommandInfo.java
@@ -4,10 +4,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Set;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface SocialLinkCommandInfo {
     String name();
+    String[] aliases() default {};
     boolean isSubComand() default false;
 }
