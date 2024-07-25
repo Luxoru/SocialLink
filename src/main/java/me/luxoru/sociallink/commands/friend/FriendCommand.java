077923a4aa6c1b72dd5eb4e4c18fbf0368cc502d
Luxoru <luxoru.dev@gmail.com>
2024-07-25 23:14:44 +0100

    made default no args return addFriend cmds

--- src/main/java/me/luxoru/sociallink/commands/friend/FriendCommand.java
+++ src/main/java/me/luxoru/sociallink/commands/friend/FriendCommand.java
@@ -1,21 +1,40 @@
 package me.luxoru.sociallink.commands.friend;
 
-import me.luxoru.sociallink.SocialLink;
 import me.luxoru.sociallink.commands.SocialLinkCommand;
 import me.luxoru.sociallink.commands.SocialLinkCommandInfo;
+import me.luxoru.sociallink.util.ArrayUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-@SocialLinkCommandInfo(name = "friend")
+@SocialLinkCommandInfo(name = "friend", aliases = {"fr"})
 public class FriendCommand extends SocialLinkCommand {
 
-    public FriendCommand(SocialLink plugin) {
-        registerSubCommand("add", new AddFriendCommand(plugin));
+    public FriendCommand() {
+        registerSubCommand(new AddFriendCommand());
+        registerSubCommand(new AcceptFriendCommand());
+        registerSubCommand(new RemoveFriendCommand());
+        registerSubCommand(new ListFriendCommand());
+
+
     }
 
     @Override
     public boolean execute(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Invalid number of args");
-        return false;
+        if(args.length < 1){
+            return getCommand("add").execute(sender, command, label, args);
+        }
+        String commandName = args[0];
+
+        SocialLinkCommand cmd = getCommand(commandName);
+        if(cmd == null) {
+            return getCommand("add").execute(sender, command, label, args);
+        }
+
+
+        return cmd.execute(sender, command, label, ArrayUtils.removeFirstElement(args));
+
     }
+
+
+
 }
